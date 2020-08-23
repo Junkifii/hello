da6165da23980097bc905dd902cdb7503f4d0007
Junkifii <69882147+Junkifii@users.noreply.github.com>
2020-08-23 18:16:23 -0500

    A Hello World Program
    Hello World Program moved to local repository

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Blake Callahan
+ *         bcallahan6@husker.unl.edu
+ * Date: 2020/08/18
+ *
+ * A simple hello world program in C, but instead of saying 'hello world' it says my name and major
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Blake Callahan\n");
+  printf("Computer Engineering\n");
+
+  return 0;
+}
